f260c1c05b383d5eaaf66933361a83cfb0ac70b5
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-05-21 08:52:34 +0600

    Vercel Config added

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const dataCollection = client.db("toyCars").collection("allData");
 
@@ -100,6 +100,7 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
